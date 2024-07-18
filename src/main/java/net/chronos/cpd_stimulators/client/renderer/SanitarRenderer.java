1b4af3d54e017de5a7bc81180fdf331e3a3840b7
Helge <u3gnjj3a@duck.com>
2024-07-18 08:03:58 +0300

    first step into animated humanoid

--- src/main/java/net/chronos/cpd_stimulators/client/renderer/SanitarRenderer.java
+++ src/main/java/net/chronos/cpd_stimulators/client/renderer/SanitarRenderer.java
@@ -1,12 +1,36 @@
 package net.chronos.cpd_stimulators.client.renderer;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
 import net.chronos.cpd_stimulators.entity.custom.Sanitar;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.IllagerModel;
+import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
+import net.minecraft.client.renderer.entity.IllagerRenderer;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
+import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.entity.monster.Pillager;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
+
+//public class SanitarRenderer extends IllagerRenderer<Pillager> {
+//    public SanitarRenderer(EntityRendererProvider.Context context) {
+//        super(context, new IllagerModel<>(context.bakeLayer(ModelLayers.PILLAGER)), 0.5F);
+//        this.addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()));
+//    }
+//
+//    public ResourceLocation getTextureLocation(Pillager p_115720_) {
+//        return ResourceLocation.fromNamespaceAndPath("cpd_stimulators", "textures/entity/sanitar.png");
+//    }
+//}
 
 public class SanitarRenderer extends HumanoidMobRenderer<Sanitar, HumanoidModel<Sanitar>> {
     public SanitarRenderer(EntityRendererProvider.Context context) {
@@ -18,4 +42,60 @@ public class SanitarRenderer extends HumanoidMobRenderer<Sanitar, HumanoidModel<
     public ResourceLocation getTextureLocation(Sanitar entity) {
         return ResourceLocation.fromNamespaceAndPath("cpd_stimulators", "textures/entity/sanitar.png");
     }
+
+	@Override
+	public void render(Sanitar entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
+		setModelStates(entity);
+		super.render(entity, entityYaw, partialTicks, poseStack, buffer, packedLight);
+	}
+
+	private void setModelStates(Sanitar entity) {
+		model.setAllVisible(true);
+
+		model.hat.visible = false;
+		model.crouching = entity.isCrouching();
+		HumanoidModel.ArmPose mainHandArmPose = getArmPose(entity, InteractionHand.MAIN_HAND);
+		HumanoidModel.ArmPose offhandArmPose = getArmPose(entity, InteractionHand.OFF_HAND);
+
+		if (mainHandArmPose.isTwoHanded())
+			offhandArmPose = entity.getOffhandItem().isEmpty() ? HumanoidModel.ArmPose.EMPTY : HumanoidModel.ArmPose.ITEM;
+
+		if (entity.getMainArm() == HumanoidArm.RIGHT) {
+			model.rightArmPose = mainHandArmPose;
+			model.leftArmPose = offhandArmPose;
+		}
+		else {
+			model.rightArmPose = offhandArmPose;
+			model.leftArmPose = mainHandArmPose;
+		}
+	}
+
+	private static HumanoidModel.ArmPose getArmPose(Sanitar entity, InteractionHand hand) {
+		ItemStack stack = entity.getItemInHand(hand);
+
+		if (stack.isEmpty())
+			return HumanoidModel.ArmPose.EMPTY;
+
+		if (entity.getUsedItemHand() == hand && entity.getUseItemRemainingTicks() > 0) {
+			UseAnim useaction = stack.getUseAnimation();
+
+			if (useaction == UseAnim.BLOCK)
+				return HumanoidModel.ArmPose.BLOCK;
+
+			if (useaction == UseAnim.BOW)
+				return HumanoidModel.ArmPose.BOW_AND_ARROW;
+
+			if (useaction == UseAnim.SPEAR)
+				return HumanoidModel.ArmPose.THROW_SPEAR;
+
+			if (useaction == UseAnim.CROSSBOW && hand == entity.getUsedItemHand())
+				return HumanoidModel.ArmPose.CROSSBOW_CHARGE;
+
+		}
+		else if (!entity.swinging && stack.getItem() instanceof CrossbowItem && CrossbowItem.isCharged(stack)) {
+			return HumanoidModel.ArmPose.CROSSBOW_HOLD;
+		}
+
+		return HumanoidModel.ArmPose.ITEM;
+	}
 }
--- src/main/java/net/chronos/cpd_stimulators/entity/custom/Sanitar.java
+++ src/main/java/net/chronos/cpd_stimulators/entity/custom/Sanitar.java
@@ -3,40 +3,51 @@ package net.chronos.cpd_stimulators.entity.custom;
 import net.chronos.cpd_stimulators.CPDStimulators;
 import net.chronos.cpd_stimulators.entity.ModEntities;
 import net.chronos.cpd_stimulators.sound.ModSounds;
+import net.minecraft.client.particle.FireworkParticles;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
-import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.CrossbowAttackMob;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.UUID;
+
 @EventBusSubscriber(modid = CPDStimulators.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
-public class Sanitar extends Monster {
+public class Sanitar extends Monster implements CrossbowAttackMob {
+    private static final EntityDataAccessor<Boolean> IS_CHARGING_CROSSBOW = SynchedEntityData.defineId(Sanitar.class, EntityDataSerializers.BOOLEAN);
+    private UUID leadingEntity;
+
     public Sanitar(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
-        setHealth(100f);
-        setAbsorptionAmount(50f);
-        setNoAi(false);
     }
 
     @SubscribeEvent
@@ -47,22 +58,18 @@ public class Sanitar extends Monster {
     @Override
     protected void registerGoals() {
         super.registerGoals();
-        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2, false) {
-            @Override
-            protected boolean canPerformAttack(LivingEntity entity) {
-                return this.isTimeToAttack() && this.mob.distanceToSqr(entity) < (this.mob.getBbWidth() * this.mob.getBbWidth() + entity.getBbWidth()) && this.mob.getSensing().hasLineOfSight(entity);
-            }
-        });
-        this.goalSelector.addGoal(2, new RandomStrollGoal(this, 1));
-        this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
-        this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(5, new FloatGoal(this));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, 1, true, false, living -> living.hasEffect(MobEffects.BAD_OMEN)));
+        this.goalSelector.addGoal(2, new RangedCrossbowAttackGoal(this, 5.0d, 16.0f));
+        this.goalSelector.addGoal(3, new RandomStrollGoal(this, 1));
+        this.targetSelector.addGoal(4, new HurtByTargetGoal(this));
+        this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(6, new FloatGoal(this));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes()
                 .add(Attributes.MOVEMENT_SPEED, .3f)
-                .add(Attributes.MAX_HEALTH, 10f)
+                .add(Attributes.MAX_HEALTH, 20f)
                 .add(Attributes.ARMOR, 0f)
                 .add(Attributes.ATTACK_DAMAGE, 3f)
                 .add(Attributes.FOLLOW_RANGE, 16f);
@@ -74,6 +81,16 @@ public class Sanitar extends Monster {
     }
 
     @Override
+    public Pose getPose() {
+        return Pose.SHOOTING;
+    }
+
+    @Override
+    public void setPose(Pose pose) {
+        super.setPose(Pose.SHOOTING);
+    }
+
+    @Override
     public SoundEvent getDeathSound() {
         return ModSounds.SANITAR_DEATH.get();
     }
@@ -82,10 +99,8 @@ public class Sanitar extends Monster {
     @Override
     public SoundEvent getAmbientSound() {
         double rnd = Math.random();
-        if (rnd < 0.25) {
-            return ModSounds.SANITAR_MUTTER.get();
-        }
-        return null;
+        if (rnd >= 0.25) return null;
+        return ModSounds.SANITAR_MUTTER.get();
     }
 
     @Override
@@ -94,12 +109,12 @@ public class Sanitar extends Monster {
     }
 
     @Override
-    public boolean shouldShowName() {
-        return true;
+    protected float getSoundVolume() {
+        return 1f;
     }
 
     @Override
-    public boolean shouldBeSaved() {
+    public boolean shouldShowName() {
         return true;
     }
 
@@ -113,15 +128,47 @@ public class Sanitar extends Monster {
         return super.level();
     }
 
+    @Nullable
     @Override
-    public ItemStack getMainHandItem() {
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType spawnType, @Nullable SpawnGroupData spawnGroupData) {
         ItemStack crossbow = new ItemStack(Items.CROSSBOW);
 
         RegistryAccess registryAccess = level().registryAccess();
         HolderLookup.RegistryLookup<Enchantment> lookup = registryAccess.lookupOrThrow(Registries.ENCHANTMENT);
-        Holder.Reference<Enchantment> enchantment = lookup.get(Enchantments.MULTISHOT).get();
+        Holder.Reference<Enchantment> multishot = lookup.get(Enchantments.MULTISHOT).get();
+        Holder.Reference<Enchantment> quick_charge = lookup.get(Enchantments.QUICK_CHARGE).get();
 
-        crossbow.enchant(enchantment ,1);
-        return crossbow;
+        crossbow.enchant(multishot ,1);
+        crossbow.enchant(quick_charge ,5);
+
+        this.setItemInHand(InteractionHand.MAIN_HAND, crossbow);
+
+        return super.finalizeSpawn(level, difficulty, spawnType, spawnGroupData);
+    }
+
+    @Override
+    public void onCrossbowAttackPerformed() {
+        CPDStimulators.LOGGER.info("shot_1");
+    }
+
+    @Override
+    public void performRangedAttack(LivingEntity livingEntity, float p_82196_2_) {
+        performCrossbowAttack(this, 2.0f);
+        CPDStimulators.LOGGER.info("shot_2");
+    }
+
+    @Override
+    protected void defineSynchedData(SynchedEntityData.Builder pBuilder){
+        super.defineSynchedData(pBuilder);
+        pBuilder.define(IS_CHARGING_CROSSBOW, false);
+    }
+
+    public boolean isChargingCrossbow() {
+        return entityData.get(IS_CHARGING_CROSSBOW);
+    }
+
+    @Override
+    public void setChargingCrossbow(boolean p_213671_1_){
+        entityData.set(IS_CHARGING_CROSSBOW, p_213671_1_);
     }
 }
